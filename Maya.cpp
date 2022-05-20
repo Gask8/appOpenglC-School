a44c5272ae6622f42cfe116eec74295acadfb26e
Gabriel Salandra <ga.salandra@gmail.com>
2022-05-20 13:00:51 -0500

    Ya con link pero hay un error

--- Maya.cpp
+++ Maya.cpp
@@ -3,17 +3,23 @@
 
 #include <math.h>
 #define size 50
-#define mayaX 20
-#define mayaY 20
+#define mayaX 10
+#define mayaY 10
 #define GRAVEDAD -9.8
 #define MASA 2000
-int sizeN = size*-1;
+#define RESTINGD 6
+#define STIFFNESS 1
+#define CURTAINSEN 50
+const int sizeN = size*-1;
+const int numLin = mayaX*(mayaX-1)+mayaY*(mayaY-1);
 using namespace std;
 
 void ajusta(int, int);
 void teclado(unsigned char, int, int);
 void circulo(float, float, int);
 void inicializaP(struct point *p, int, int);
+void inicializaL(struct link *l, struct point *p1, struct point *p2);
+void inicializador();
 void dibujaPuntos();
 void dibujaMaya();
 
@@ -22,6 +28,16 @@ struct point{
 	double vel[2];
 	double last[2]; //Used in Inertia
 	double fX;
+	struct link *lin[4];
+	int cont;
+};
+struct link{
+	struct point *p1;
+	struct point *p2;
+	float restingDistance;
+  	float stiffness;
+  	float tearSensitivity;
+  	bool roto;
 };
 
 double h= 0.025; // h incrementos de tiempo
@@ -29,9 +45,27 @@ GLboolean bx=GL_FALSE;
 GLboolean by=GL_FALSE;
 
 struct point points[mayaX][mayaY];
-GLubyte paleta[7][3]={{0,0,255},{0,255,255},{255,0,255},{0,255,0},{255,0,0},{50,40,80},{0,100,100}};
+struct link links[numLin];
 
 //Fuerzas==========================================
+void fuerzaCuerda(struct link *l){
+	float diffX = l->p1->pos[0] - l->p2->pos[0];
+    float diffY = l->p1->pos[1] - l->p2->pos[1];
+    float d = sqrt(diffX * diffX + diffY * diffY);
+    float difference = (l->restingDistance - d) / d;
+    
+    if (d > l->tearSensitivity) l->roto=false;
+    	
+    float im1 = 1 / MASA;
+    float im2 = 1 / MASA;
+    float scalarP1 = (im1 / (im1 + im2)) * STIFFNESS;
+    float scalarP2 = STIFFNESS - scalarP1;
+
+    l->p1->pos[0] += diffX * scalarP1 * difference;
+    l->p1->pos[1] += diffY * scalarP1 * difference;
+    l->p2->pos[0] -= diffX * scalarP2 * difference;
+    l->p2->pos[1] -= diffY * scalarP2 * difference;
+}
 void integraVerlet(struct point *p){
 	//Inertia
 	double accX =0,accY=0;
@@ -61,6 +95,11 @@ void integraEuler(struct point *p){
 }
 void constraints(struct point *p){
 	//colisiones
+	int i;
+//	for (i=0; i < p->cont; i++) {
+//		fuerzaCuerda(p->lin[i]);
+//	}
+	
 	if(p->pos[0]>=size || p->pos[0]<=sizeN){ //X
 		p->fX=-1*p->fX;
 //		point.velocidad[0]=0;
@@ -84,7 +123,8 @@ void dibuja(void){
 	glClear(GL_COLOR_BUFFER_BIT);
 	
     glPointSize(2);
-    glColor3ubv(paleta[0]);
+    glColor3ub(0,0,0);
+    dibujaPuntos();
     dibujaMaya();
     
     glutSwapBuffers();
@@ -92,6 +132,7 @@ void dibuja(void){
 void anima(void){
 //	integraEuler();
 	int i,j;
+	
 	for(i=0;i<mayaX;i++){
 		for(j=0;j<mayaY;j++){
 			integraVerlet(&points[i][j]);
@@ -102,7 +143,7 @@ void anima(void){
 	glutPostRedisplay();
 }
 
-//Main ========================================<<
+//Main ========================================<<<<<<<<<<<<<<<<<<<<<<<
 int main(int argc, char** argv){
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE);
@@ -112,14 +153,9 @@ int main(int argc, char** argv){
 	glutReshapeFunc(ajusta);
 	glutKeyboardFunc(teclado);
 	glutIdleFunc(anima);
-	
-	int i,j,ix,jx;
-	for(i=0,ix=mayaX;i<mayaX;i++,ix-=2){
-		for(j=0,jx=mayaY;j<mayaY;j++,jx-=2){
-			inicializaP(&points[i][j], ix, jx);
-		}
-	}
-	
+
+	inicializador();
+		
 	glutMainLoop();
 	return 0;
 }
@@ -159,24 +195,18 @@ void dibujaPuntos(){
 }
 void dibujaMaya(){    
     int i,j;
-	for(i=0;i<mayaX;i++){
-		glBegin(GL_LINE_STRIP);
-		for(j=0;j<mayaY;j++){
-			glVertex2f(points[i][j].pos[0],points[i][j].pos[1]);
+	int c =0;
+	glBegin(GL_LINES);
+	for(c=0;c<numLin;c++){
+		if(links[c].roto){
+			glVertex2f(links[c].p1->pos[0],links[c].p1->pos[1]);
+			glVertex2f(links[c].p2->pos[0],links[c].p2->pos[1]);	
 		}
-		glEnd();
 	}
-	
-	for(i=0;i<mayaX;i++){
-		glBegin(GL_LINE_STRIP);
-		for(j=0;j<mayaY;j++){
-			glVertex2f(points[j][i].pos[0],points[j][i].pos[1]);
-		}
-		glEnd();
-	}
+	glEnd();
     
 }
-//Funciones Otras ====================================
+//Funciones Inicializadoras ====================================
 void inicializaP(struct point *p, int x, int y){
 	p->pos[0]=x;
 	p->pos[1]=y;
@@ -185,7 +215,40 @@ void inicializaP(struct point *p, int x, int y){
 	p->vel[0]=0;
 	p->vel[1]=0;
 	p->fX = 0.001;
-//	cout<<"x:"<<x<<endl;
-//	cout<<"y:"<<y<<endl;
+	p->cont=0;
+}
+void inicializaL(struct link *l, struct point *p1a, struct point *p2a){
+	l->p1=p1a;
+	l->p1->lin[l->p1->cont]=l;
+	l->p1->cont++;
+	l->p2=p2a;
+	l->p2->lin[l->p2->cont]=l;
+	l->p2->cont++;
+	l->restingDistance=RESTINGD;
+	l->stiffness=STIFFNESS;
+	l->tearSensitivity=CURTAINSEN;
+	l->roto=true;
+}
+void inicializador(){
+	int i,j,ix,jx;
+	for(i=0,ix=mayaX;i<mayaX;i++,ix-=2){
+		for(j=0,jx=mayaY;j<mayaY;j++,jx-=2){
+			inicializaP(&points[i][j], ix, jx);
+		}
+	}
+	
+	int c=0;
+	for(i=0;i<mayaX;i++){
+		for(j=0;j<mayaY-1;j++){
+			inicializaL(&links[c], &points[i][j], &points[i][j+1]);
+			c++;
+		}
+	}
+	for(i=0;i<mayaX-1;i++){
+		for(j=0;j<mayaY;j++){
+			inicializaL(&links[c], &points[i][j], &points[i+1][j]);
+			c++;
+		}
+	}
 }
 
